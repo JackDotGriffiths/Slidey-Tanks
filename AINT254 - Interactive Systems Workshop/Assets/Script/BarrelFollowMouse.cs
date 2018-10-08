999aaa88c70a053b38786eeff68c98bbfba5a5ec
JackDotGriffiths <32262182+JackDotGriffiths@users.noreply.github.com>
2018-10-08 14:25:40 +0100

    Whiteblock tank created - Started code

---
+++ AINT254 - Interactive Systems Workshop/Assets/Script/BarrelFollowMouse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelFollowMouse : MonoBehaviour {
+    public Vector3 mousePosition;
+	void Update () {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+}
