0962e76b452562afc5fc072403c978d59798cc0b
JackDotGriffiths <32262182+JackDotGriffiths@users.noreply.github.com>
2018-10-08 15:35:32 +0100

    Tank Feature 1 complete
    Barrel now rotates towards mouse and shoots appropriate bullets.

--- AINT254 - Interactive Systems Workshop/Assets/Script/BarrelFollowMouse.cs
+++ AINT254 - Interactive Systems Workshop/Assets/Script/BarrelFollowMouse.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BarrelFollowMouse : MonoBehaviour {
-    public Vector3 mousePosition;
-	void Update () {
-        mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    public Vector3 RayHitPoint;
+    void Update () {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            RayHitPoint = hit.point;
+        }
+
+        transform.LookAt(new Vector3(RayHitPoint.x, transform.position.y, RayHitPoint.z));
+
     }
 }
---
+++ AINT254 - Interactive Systems Workshop/Assets/Script/BulletMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireBullet();
+        }
+    }
+
+
+	// Use this for initialization
+	void FireBullet () {
+        var bullet = (GameObject)Instantiate(
+        bulletPrefab,
+        bulletSpawn.position,
+        bulletSpawn.rotation);
+
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * 7;
+
+    }
+}
