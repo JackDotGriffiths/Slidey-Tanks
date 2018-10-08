a3764108430ae411a7d7f272a99351e0a6c5f321
JackDotGriffiths <32262182+JackDotGriffiths@users.noreply.github.com>
2018-10-08 15:45:26 +0100

    Tank Slide Complete
    Game now exerts a force opposite of the direction of shooting onto the tank. This slides it around the map.

---
+++ AINT254 - Interactive Systems Workshop/Assets/Script/TankRecoil.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankRecoil : MonoBehaviour {
+    public Vector3 mousePosition;
+    public Rigidbody Tank;
+
+    public Vector3 RayHitPoint;
+    public Vector3 MouseTarget;
+    public Vector3 recoilDirection;
+
+	void Update () {
+        GetMousePos();
+		if (Input.GetMouseButtonDown(0))
+        {
+            Recoil();
+        }
+	}
+
+    void GetMousePos()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            RayHitPoint = hit.point;
+        }
+
+        MouseTarget = new Vector3(RayHitPoint.x, transform.position.y, RayHitPoint.z);
+
+        //Reverses the Vector as to give the direction of the recoil push.
+        recoilDirection = new Vector3(MouseTarget.x * -1, MouseTarget.y * -1, MouseTarget.z * -1);
+    }
+    void Recoil()
+    {
+        Tank.AddForce(recoilDirection * 50);
+    }
+}
