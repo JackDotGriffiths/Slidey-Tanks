9d3d05837b66d1a14f82e7ecf1b16a8442a18bfe
JackDotGriffiths <32262182+JackDotGriffiths@users.noreply.github.com>
2018-10-16 10:34:10 +0100

    PlayerSelect screen complete
    Not yet fully implemented as I need to complete the level design and make a second scene for singleplayer

---
+++ AINT254 - Interactive Systems Workshop/Assets/Script/UI Menu Controls/OnePlayerEffects.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnePlayerEffects : MonoBehaviour,IPointerEnterHandler ,IPointerExitHandler {
+
+    public GameObject tank;
+    public Light spotlight;
+
+    private bool onHover = false;
+	
+	// Update is called once per frame
+	void Update () {
+        if (onHover == true)
+        {
+            tank.transform.Rotate(Vector3.up * Time.deltaTime * 80);
+            spotlight.enabled = true;
+        }
+        else
+        {
+            spotlight.enabled = false;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        onHover = true;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        onHover = false;
+    }
+}
