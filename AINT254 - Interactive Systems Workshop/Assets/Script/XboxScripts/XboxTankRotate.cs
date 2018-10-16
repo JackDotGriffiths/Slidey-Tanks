f5efee54b0da939dfe74c19732a498be1be3b78c
JackDotGriffiths <32262182+JackDotGriffiths@users.noreply.github.com>
2018-10-16 11:44:06 +0100

    Installed Controller Rotation and Refined Bullet Particle

---
+++ AINT254 - Interactive Systems Workshop/Assets/Script/XboxScripts/XboxTankRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XboxTankRotate : MonoBehaviour {
+
+    private Vector3 lookdirection;
+    public float f_vertical;
+    public float f_horizontal;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        f_vertical = Input.GetAxis("Player2Vertical2") * 360;
+        f_horizontal = Input.GetAxis("Player2Horizontal2") * 360;
+
+        lookdirection = new Vector3(f_horizontal, transform.position.y, f_vertical);
+
+        transform.LookAt(lookdirection);
+    }
+}
