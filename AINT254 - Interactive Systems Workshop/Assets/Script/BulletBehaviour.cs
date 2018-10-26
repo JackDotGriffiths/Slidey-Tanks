9c0f8b98a30b66cbfd5d4f0fc2f9088095cb3847
Jack Griffiths <32262182+JackDotGriffiths@users.noreply.github.com>
2018-10-26 13:34:34 +0100

    Quick bug Fixes
    Bullet collision with another bullet results in explosion (Not fully working) Collision with grey wall area increased

--- AINT254 - Interactive Systems Workshop/Assets/Script/BulletBehaviour.cs
+++ AINT254 - Interactive Systems Workshop/Assets/Script/BulletBehaviour.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class BulletBehaviour: MonoBehaviour {
 
     public GameObject destroyEffect;
+    public GameObject bullet1;
+    public GameObject bullet2;
 
 
     private void OnTriggerEnter(Collider other)
     {
         //Destroy bullet when it collides with walls.
-        if(other.tag == "Walls")
+        if(other.tag == "Walls" || other.tag == "Player1Bullet" || other.tag == "Player2Bullet")
         {
             var destroyParticles = (GameObject)Instantiate(
             destroyEffect,
